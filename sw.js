4b15fd27c79a3594a2952f7600f9b2462bef8774
farhanamomin <102279699+farhanamomin@users.noreply.github.com>
2026-03-15 03:30:27 +0530

    Create sw.js service worker file for offline PWA support with caching functionality

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'sadaqah-jariyah-v1';
+const CACHE_ASSETS = [
+    './index.html',
+    './manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(CACHE_ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                })
+            );
+        })
+    );
+});
